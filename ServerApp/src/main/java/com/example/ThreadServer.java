463006f5ecd04e470f5a03cbcce029bd1e6fb61d
Davide Aiazzi <aiazzi.davide@itismeucci.com>
2023-11-05 15:05:23 +0100

    threadUpdate2

--- ServerApp/src/main/java/com/example/ThreadServer.java
+++ ServerApp/src/main/java/com/example/ThreadServer.java
@@ -18,7 +18,7 @@ public class ThreadServer extends Thread {
     String ANSI_GREEN = "\u001B[32m";
     String ANSI_RED = "\u001B[31m";
     String ANSI_BLUE = "\u001B[34m";
-    String ANSi_RESET = "\u001B[0m";
+    String ANSI_RESET = "\u001B[0m";
 
     
 
@@ -34,46 +34,46 @@ public class ThreadServer extends Thread {
             outVersoClient.writeBytes("Benvenuto, inserisci la difficoltà (default: 100)" + '\n');
 
             stringaRicevuta = inDalClient.readLine();
-            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  Ricevuta la stringa dal client : " + stringaRicevuta);
+            System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName() + ":  Ricevuta la stringa dal client : " + stringaRicevuta);
             try {
                 difficolta = Integer.parseInt(stringaRicevuta);
             } catch (Exception e) {
-                System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  difficoltà default: 100");
+                System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName() + ":  difficoltà default: 100");
                 difficolta = 100;
             }
             
             randomNum = randomNumber(difficolta);
-            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":\u001B[34m numero generato: " + randomNum);
+            System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName() + ":\u001B[34m numero generato: " + randomNum);
             outVersoClient.writeBytes("inserisci un numero" + '\n');
 
             while (!win) {
             //attendo stringa dal client
             stringaRicevuta = inDalClient.readLine();
 
-            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  Ricevuta la stringa dal client " + client.getPort() + ":  "+ ANSI_GREEN + stringaRicevuta);
+            System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName() + ":  Ricevuta la stringa dal client " + client.getPort() + ":  "+ ANSI_GREEN + stringaRicevuta);
 
             //elaboro la stringa
             risposta = elaboraStringa(stringaRicevuta);
 
             if (risposta.equals("quit")) {
-                System.out.println(ANSi_RESET + "THREAD"+ this.threadId()+ ANSI_RED + ":  Chiusura connessione");
+                System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName()+ ANSI_RED + ":  Chiusura connessione");
                 client.close();
                 System.exit(1);
             }
 
             //rispondo al client
-            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  Invio la risposta al client" + client.getInetAddress());
+            System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName() + ":  Invio la risposta al client" + client.getInetAddress());
             outVersoClient.writeBytes(risposta+ '\n');
         } 
 
         client.close();
         }catch (SocketException e) {
-            System.out.println(ANSi_RESET + "THREAD"+ this.threadId()+ ANSI_RED + ":  Connessione chiusa dal client!");
+            System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName()+ ANSI_RED + ":  Connessione chiusa dal client!");
             System.exit(1);
 
         }catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println(ANSi_RESET + "THREAD"+ this.threadId()+ ANSI_RED + ":  qualcosa è andato storto!");
+            System.out.println(ANSI_RESET + "THREAD"+ currentThread().getName()+ ANSI_RED + ":  qualcosa è andato storto!");
             System.exit(1);
         }
     }
@@ -89,10 +89,10 @@ public class ThreadServer extends Thread {
                     return ANSI_GREEN + "Hai indovinato! con " + tentativi + " tentativi";
                 } else if (numero > randomNum) {
                     tentativi++;
-                    return ANSI_RED + "Troppo alto!";
+                    return ANSI_RED + "Troppo alto!" + ANSI_RESET;
                 } else {
                     tentativi++;
-                    return ANSI_RED+"Troppo basso!";
+                    return ANSI_RED + "Troppo basso!" + ANSI_RESET;
                 }
             } catch (Exception e) {
                 return "Non hai inserito un numero!";
