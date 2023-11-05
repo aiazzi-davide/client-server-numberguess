3ce3b741d0430ae43e2e98a128062a303ff830ef
Davide Aiazzi <aiazzi.davide@itismeucci.com>
2023-11-05 13:58:32 +0100

    threadUpdate

--- ServerApp/src/main/java/com/example/ThreadServer.java
+++ ServerApp/src/main/java/com/example/ThreadServer.java
@@ -34,46 +34,46 @@ public class ThreadServer extends Thread {
             outVersoClient.writeBytes("Benvenuto, inserisci la difficoltà (default: 100)" + '\n');
 
             stringaRicevuta = inDalClient.readLine();
-            System.out.println("THREAD"+ this.threadId() + ":  Ricevuta la stringa dal client : " + stringaRicevuta);
+            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  Ricevuta la stringa dal client : " + stringaRicevuta);
             try {
                 difficolta = Integer.parseInt(stringaRicevuta);
             } catch (Exception e) {
-                System.out.println("THREAD"+ this.threadId() + ":  difficoltà default: 100");
+                System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  difficoltà default: 100");
                 difficolta = 100;
             }
             
             randomNum = randomNumber(difficolta);
-            System.out.println("THREAD"+ this.threadId() + ":\u001B[34m numero generato: " + randomNum);
+            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":\u001B[34m numero generato: " + randomNum);
             outVersoClient.writeBytes("inserisci un numero" + '\n');
 
             while (!win) {
             //attendo stringa dal client
             stringaRicevuta = inDalClient.readLine();
 
-            System.out.println("THREAD"+ this.threadId() + ":  Ricevuta la stringa dal client " + client.getPort() + ":  "+ ANSI_GREEN + stringaRicevuta);
+            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  Ricevuta la stringa dal client " + client.getPort() + ":  "+ ANSI_GREEN + stringaRicevuta);
 
             //elaboro la stringa
             risposta = elaboraStringa(stringaRicevuta);
 
             if (risposta.equals("quit")) {
-                System.out.println("THREAD"+ this.threadId()+ ANSI_RED + ":  Chiusura connessione");
+                System.out.println(ANSi_RESET + "THREAD"+ this.threadId()+ ANSI_RED + ":  Chiusura connessione");
                 client.close();
                 System.exit(1);
             }
 
             //rispondo al client
-            System.out.println("THREAD"+ this.threadId() + ":  Invio la risposta al client" + client.getInetAddress());
+            System.out.println(ANSi_RESET + "THREAD"+ this.threadId() + ":  Invio la risposta al client" + client.getInetAddress());
             outVersoClient.writeBytes(risposta+ '\n');
         } 
 
         client.close();
         }catch (SocketException e) {
-            System.out.println("THREAD"+ this.threadId()+ ANSI_RED + ":  Connessione chiusa dal client!");
+            System.out.println(ANSi_RESET + "THREAD"+ this.threadId()+ ANSI_RED + ":  Connessione chiusa dal client!");
             System.exit(1);
 
         }catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("THREAD"+ this.threadId()+ ANSI_RED + ":  qualcosa è andato storto!");
+            System.out.println(ANSi_RESET + "THREAD"+ this.threadId()+ ANSI_RED + ":  qualcosa è andato storto!");
             System.exit(1);
         }
     }
@@ -86,13 +86,13 @@ public class ThreadServer extends Thread {
                 int numero = Integer.parseInt(stringaRicevuta);
                 if (numero == randomNum) {
                     win = true;
-                    return "Hai indovinato! con " + tentativi + " tentativi";
+                    return ANSI_GREEN + "Hai indovinato! con " + tentativi + " tentativi";
                 } else if (numero > randomNum) {
                     tentativi++;
-                    return "Troppo alto!";
+                    return ANSI_RED + "Troppo alto!";
                 } else {
                     tentativi++;
-                    return "Troppo basso!";
+                    return ANSI_RED+"Troppo basso!";
                 }
             } catch (Exception e) {
                 return "Non hai inserito un numero!";
